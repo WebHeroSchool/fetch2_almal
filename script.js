41c5a2b03bcc29a35d6197c7ade275cb225b86f4
Алена Маляренко <malyarenkoal@yandex.ru>
2020-11-07 14:38:01 +0600

    promise

--- script.js
+++ script.js
@@ -10,27 +10,48 @@ let getUsername = (url) => {
 }
 
 let name = getUsername(url);
-fetch('https://api.github.com/users/' + name)
-    .then(result => result.json())
-    .then(json => {
+
+let getNowDate = new Promise((resolve, reject) => {
+    let nowDate = new Date();
+    setTimeout(() => nowDate ? resolve(nowDate) : reject ('Время не определенно'), 3000)
+  });
+
+let getUserData = fetch('https://api.github.com/users/' + name);
+
+Promise.all([getUserData, getNowDate])
+    .then(([ourUserData, ourNowDate]) => {
+        userData = ourUserData;
+        currentDate = ourNowDate;
+    })
+
+    .then(result => userData.json())
+    .then(userInfo => {
       const body = document.body;
 
+      let elementForPreloader = document.getElementById('loader');
+      elementForPreloader.className = 'active';
+
       const gitHubUser = document.createElement('h2');
-      gitHubUser.innerHTML = json.login;
+      gitHubUser.innerHTML = userInfo.login;
+      gitHubUser.className='active_login';
       body.append(gitHubUser);
 
       const gitHubUserName = document.createElement('a');
-      gitHubUserName.innerHTML = json.name;
-      gitHubUserName.href = json.html_url;
+      gitHubUserName.innerHTML = userInfo.name;
+      gitHubUserName.href = userInfo.html_url;
       body.append(gitHubUserName);
 
       const gitHubUserBio = document.createElement('p');
-      gitHubUserBio.innerHTML = json.bio;
+      gitHubUserBio.innerHTML = userInfo.bio;
       body.append(gitHubUserBio);
 
       const gitHubUserAvatar = document.createElement('img');
-      gitHubUserAvatar.src = json.avatar_url;
+      gitHubUserAvatar.src = userInfo.avatar_url;
       body.append(gitHubUserAvatar);
+
+      const elementDate = document.createElement('p');
+      elementDate.innerHTML = currentDate;
+      body.appendChild(elementDate);
       })
 
 .catch(err => alert('Информация не доступна: ' + err));
